5a265c671d860ec55136a991671e145cc2370d1b
DimaKaminsckyi <dimakaminsckyi@gmail.com>
2019-02-21 19:54:59 +0200

    add upper and lower case check

--- src/main/java/PasswordGenerate.java
+++ src/main/java/PasswordGenerate.java
@@ -1,13 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class PasswordGenerate {
 
-    private final char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+    private final String LOWER = "abcdefghijklmnopqrstuvwxyz";
+    private final String UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    private Random random = new Random();
 
     public StringBuilder generate(int passwordSize){
 
         StringBuilder password = new StringBuilder();
+        List<String> symbolList = new ArrayList<String>(2);
+        symbolList.add(LOWER);
+        symbolList.add(UPPER);
 
-        for (int i = 0; i < passwordSize;i++){
-            password.append(alphabet[(int) (Math.random() * 26)]);
+        for (int i = 0; i < passwordSize ;i++){
+            String charCategory = symbolList.get(random.nextInt(symbolList.size()));
+            int position = random.nextInt(charCategory.length());
+            password.append(charCategory.charAt(position));
         }
         return password;
     }
--- src/test/java/PasswordGenerateTest.java
+++ src/test/java/PasswordGenerateTest.java
@@ -5,14 +5,26 @@ import org.junit.Test;
 public class PasswordGenerateTest {
 
     private PasswordGenerate unit;
+    private StringBuilder passwordUnit;
 
     @Before
     public void initialization(){
         unit = new PasswordGenerate();
+        passwordUnit = unit.generate(10);
     }
 
     @Test
     public void whenGenerateReturnCorrectSize(){
         Assert.assertEquals( 9 ,unit.generate(9).length());
+    }
+
+    @Test
+    public void passwordHasLowRegister(){
+        Assert.assertTrue(!passwordUnit.toString().equals(passwordUnit.toString().toUpperCase()));
+    }
+
+    @Test
+    public void passwordHasUpperRegisgter(){
+        Assert.assertTrue(!passwordUnit.toString().equals(passwordUnit.toString().toLowerCase()));
     }
 }
